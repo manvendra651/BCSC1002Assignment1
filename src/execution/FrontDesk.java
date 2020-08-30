c54594fc32e029f95fc2b9e9158ab3927b98307f
manvendra singh rathore <manvendra.rathore_cs19@gla.ac.in>
2020-08-30 18:59:55 +0530

    private fields created

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,6 +7,11 @@
 package execution;
 
 public class FrontDesk {
+    private static final int ADD_NEW_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int MY_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
+
     public static void main(String[] args) {
 
     }
