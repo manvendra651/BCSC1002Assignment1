537977c6762539978f966d4d650d92586b628269
manvendra singh rathore <manvendra.rathore_cs19@gla.ac.in>
2020-08-30 18:31:32 +0530

    getter and setter created

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -15,4 +15,27 @@ public class Book {
         this.bookName = bookName;
     }
 
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getThirteenDigitISBNNumber() {
+        return thirteenDigitISBNNumber;
+    }
+
+    public void setThirteenDigitISBNNumber(String thirteenDigitISBNNumber) {
+        this.thirteenDigitISBNNumber = thirteenDigitISBNNumber;
+    }
 }
