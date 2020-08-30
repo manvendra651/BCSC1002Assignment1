1022e1b66133bc4e79555199134deb1dd5899c86
manvendra singh rathore <manvendra.rathore_cs19@gla.ac.in>
2020-08-30 18:40:41 +0530

    consructor1 created

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -11,6 +11,14 @@ import java.util.Arrays;
 public class Library {
     private Book[] allBooksCurrentlyAvailable;
 
+    public Library() {
+        this.allBooksCurrentlyAvailable = new Book[1000];
+        this.allBooksCurrentlyAvailable = new Book[10];
+        for (int index = 0; index < allBooksCurrentlyAvailable.length; index++) {
+            allBooksCurrentlyAvailable[index] = new Book("Books" + (index + 1));
+        }
+    }
+
     public Library(Book[] allBooksCurrentlyAvailable) {
         this.allBooksCurrentlyAvailable = allBooksCurrentlyAvailable;
     }
